3afb3138a82bac954a32fb32fe8cf5b9787d6ed0
ryuya <ryuuya110@outlook.jp>
2021-02-17 18:10:20 +0900

    fixed

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Router from './components/Router';
+ 
+class App extends Component {
+  render() {
+    return (
+        <Router />
+    );
+  }
+}
+ 
+export default App;
