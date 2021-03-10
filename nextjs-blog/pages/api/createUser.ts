e9cd8ce00d5ab6f9b46c41375d2a7c14a785e30a
ryuya <ryuuya110@outlook.jp>
2021-03-11 00:55:46 +0900

    create hash

--- nextjs-blog/pages/api/createUser.ts
+++ nextjs-blog/pages/api/createUser.ts
@@ -1,6 +1,7 @@
 import { NextApiHandler } from "next";
 import prisma from "../../lib/prisma";
 import * as z from "zod";
+import crypto from 'crypto';
 
 const requestUserSchema = z.object({
   name: z.string(),
@@ -8,14 +9,20 @@ const requestUserSchema = z.object({
   password: z.string()
 });
 
-const handler: NextApiHandler = async (req, res) => {
+const handler: NextApiHandler = async (req, res,  name, password) => {
+  const salt = crypto.randomBytes(16).toString('hex')
+  const hash = crypto
+  .pbkdf2Sync(password, salt, 1000, 64, 'sha512')
+  .toString('hex')
+
   try {
     const result = requestUserSchema.parse(req.body);
     await prisma.user.create({
       data: {
         name: result.name,
         email: result.email,
-        password: result.password,
+        password: hash,
+        salt,
       },
     });
     res.json({
@@ -26,5 +33,13 @@ const handler: NextApiHandler = async (req, res) => {
     res.json({ ok: false, error });
   }
 };
+
+export function validatePassword(user:any, inputPassword:any) {
+  const inputHash = crypto
+    .pbkdf2Sync(inputPassword, user.salt, 1000, 64, 'sha512')
+    .toString('hex')
+  const passwordsMatch = user.password === inputHash
+  return passwordsMatch
+}
 
 export default handler;
