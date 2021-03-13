d6390fa569bad9a5ec19cb4d9a6f522975564780
ryuya <ryuuya110@outlook.jp>
2021-03-14 00:34:19 +0900

    add

--- nextjs-blog/lib/password-local.ts
+++ nextjs-blog/lib/password-local.ts
@@ -1,24 +1,6 @@
 import Local from 'passport-local'
-import { findUser, validatePassword } from './user'
 import crypto from 'crypto';
-
-export const localStrategy = new Local.Strategy(function (
-  name,
-  password,
-  done
-) {
-  findUser({ name })
-    .then((user) => {
-      if (user && validatePassword(user, password)) {
-        done(null, user)
-      } else {
-        done(new Error('ユーザネームとパスワードが一致しません'))
-      }
-    })
-    .catch((error) => {
-      done(error)
-    })
-})
+import prisma from './prisma';
 
 export function validatePassword(user:any, inputPassword:any) {
   const inputHash = crypto
@@ -27,3 +9,22 @@ export function validatePassword(user:any, inputPassword:any) {
   const passwordsMatch = user.password === inputHash
   return passwordsMatch
 }
+
+export const localStrategy = new Local.Strategy(async function (
+  email,
+  password,
+  done
+) {
+  const user = await prisma.user.findUnique({where:{
+    email
+  }})
+  // ! If there is no user, return error
+
+  const isPasswordValid = validatePassword(user, password)
+
+  // ! If the password is wrong, return error
+
+  if(isPasswordValid) {
+    done(null, user)
+  }
+})
